330867e810509ecbcf368cb55c967ec266cf5ab1
drakeira <thiago.p.dasilva2005@gmail.com>
2022-12-04 14:26:41 -0300

    feat: add test vefiry if search-input component call a function when typed

--- src/components/SearchInput/SearchInput.spec.tsx
+++ src/components/SearchInput/SearchInput.spec.tsx
@@ -1,15 +1,36 @@
-import { render } from "@testing-library/react"
+import { render } from "@testing-library/react";
+import userEvent from "@testing-library/user-event";
 
-const SearchInput = () => {
-    return (
-        <input type="text" data-testid='search-input-component' placeholder="digite o nome do usuário"/>
-    )
-}
+interface SearchInputProps extends React.HTMLAttributes<HTMLInputElement> {}
 
-describe('Search Input Component', () => {
-    it('should have a placeholder: "digite o nome do usuário', () => {
-        const {getByTestId} = render(<SearchInput/>)
+const SearchInput = ({ ...rest }: SearchInputProps) => {
+  return (
+    <input
+      type="text"
+      data-testid="search-input-component"
+      placeholder="digite o nome do usuário"
+      {...rest}
+    />
+  );
+};
 
-        expect(getByTestId('search-input-component')).toHaveAttribute('placeholder', 'digite o nome do usuário')
-    })
-})
+describe("Search Input Component", () => {
+  it("should call a function when user type on input", async () => {
+    const mockFunction = jest.fn();
+    const { getByTestId, debug } = render(
+      <SearchInput onChange={() => mockFunction()} />
+    );
+
+    await userEvent.type(getByTestId("search-input-component"), "any_text");
+
+    expect(mockFunction).toHaveBeenCalled();
+  });
+  it('should have a placeholder: "digite o nome do usuário', () => {
+    const { getByTestId } = render(<SearchInput />);
+
+    expect(getByTestId("search-input-component")).toHaveAttribute(
+      "placeholder",
+      "digite o nome do usuário"
+    );
+  });
+});
