41fcdc7fb100bc970f9ac5a428dca73ef176ce06
drakeira <thiago.p.dasilva2005@gmail.com>
2022-12-04 18:49:15 -0300

    feat: add some test to verify if elements exists in document

--- src/components/GettedGitHubProfile/GettedGitHubProfile.spec.tsx
+++ src/components/GettedGitHubProfile/GettedGitHubProfile.spec.tsx
@@ -6,6 +6,18 @@ const GettedGitHubProfile = () => {
             <div>
                 <img src="#" alt="github-profile-image" data-testid='github-profile-image-element'/>
             </div>
+            <div>
+                <h1 data-testid='github-profile-name-element'>Nome usuário</h1>
+                <span data-testid='github-profile-username-element'>Usrname</span>
+                <div>
+                    <p data-testid='github-profile-bio-element'>
+
+                    </p>
+                </div>
+            </div>
+            <div>
+                <h3 data-testid='github-profile-languages-element'>Linguagens</h3>
+            </div>
         </section>
     )
 }
@@ -16,5 +28,17 @@ describe('Getted Git Hub Profile', () => {
 
         expect(getByTestId('github-profile-image-element')).toBeInTheDocument()
         expect(getByTestId('github-profile-image-element')).toHaveAttribute('alt', 'github-profile-image')
+    })
+    it('should have a div for github user name, and sub divs: username, bio', () => {
+        const {getByTestId} = render(<GettedGitHubProfile/>)
+
+        expect(getByTestId('github-profile-name-element')).toBeInTheDocument()
+        expect(getByTestId('github-profile-username-element')).toBeInTheDocument()
+        expect(getByTestId('github-profile-bio-element')).toBeInTheDocument()
+    })
+    it('should have a div for github user languages', () => {
+        const {getByTestId} = render(<GettedGitHubProfile/>)
+
+        expect(getByTestId('github-profile-languages-element')).toBeInTheDocument()
     })
 })
