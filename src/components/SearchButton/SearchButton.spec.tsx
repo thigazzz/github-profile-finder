d44c823db13c881f73f2fea26e64e6c12afdb0be
drakeira <thiago.p.dasilva2005@gmail.com>
2022-12-04 18:29:00 -0300

    feat: add test if search-button component call a function when click

--- src/components/SearchButton/SearchButton.spec.tsx
+++ src/components/SearchButton/SearchButton.spec.tsx
@@ -1,14 +1,24 @@
 import { render } from "@testing-library/react"
+import userEvent from '@testing-library/user-event'
+import React from "react"
 
-const SearchButton = () => {
+const SearchButton = ({...rest}: React.ButtonHTMLAttributes<HTMLButtonElement>) => {
     return (
-        <button>
+        <button {...rest} data-testid='button-test'>
             Pesquisar
         </button>
     )
 }
 
 describe('Search Button Component', () => {
+    it('should call a function when button was clicked', async () => {
+        const mockHandleClickFunction = jest.fn()
+        const {getByTestId} = render(<SearchButton onClick={mockHandleClickFunction}/>)
+
+        await userEvent.click(getByTestId('button-test'))
+
+        expect(mockHandleClickFunction).toHaveBeenCalled()
+    })
     it('should have a content text: Pesquisar (isso irá mudar mais tarde)', () => {
         const {container} = render(<SearchButton/>)
 
