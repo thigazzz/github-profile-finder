f34857236b8c56ac5a0e4332a77a3b35eefd2c2b
drakeira <thiago.p.dasilva2005@gmail.com>
2022-12-08 16:23:18 -0300

    feat: add set loading false when all requests is finish

--- src/pages/Main/handles/handleSearchUser.ts
+++ src/pages/Main/handles/handleSearchUser.ts
@@ -1,18 +1,31 @@
 import React, { SetStateAction } from "react";
-import { GithubUserData, receiveGithubUserProfileData } from "../functions/receiveGithubUserProfileData";
+import {
+  GithubUserData,
+  receiveGithubUserProfileData,
+} from "../functions/receiveGithubUserProfileData";
 
-export const handleSearchUser = async (username: string, setState: React.Dispatch<SetStateAction<GithubUserData | undefined>>) => {
-    const receivedUsernameProfileData = await receiveGithubUserProfileData(username)
+export const handleSearchUser = async (
+  username: string,
+  setState: React.Dispatch<SetStateAction<GithubUserData | undefined>>,
+  setLoading: React.Dispatch<SetStateAction<boolean>>
+) => {
+  setLoading(true)
 
-    setState({
-      name: receivedUsernameProfileData.name,
-      username: receivedUsernameProfileData.username,
-      avatar_url: receivedUsernameProfileData.avatar_url,
-      biography: receivedUsernameProfileData.biography,
-      languages: receivedUsernameProfileData.languages,
-      followers: receivedUsernameProfileData.followers,
-      following: receivedUsernameProfileData.following,
-      repositories: receivedUsernameProfileData.repositories,
-      stars: receivedUsernameProfileData.stars,
-    });
-  };
+  const receivedUsernameProfileData = await receiveGithubUserProfileData(
+    username
+  );
+
+  setState({
+    name: receivedUsernameProfileData.name,
+    username: receivedUsernameProfileData.username,
+    avatar_url: receivedUsernameProfileData.avatar_url,
+    biography: receivedUsernameProfileData.biography,
+    languages: receivedUsernameProfileData.languages,
+    followers: receivedUsernameProfileData.followers,
+    following: receivedUsernameProfileData.following,
+    repositories: receivedUsernameProfileData.repositories,
+    stars: receivedUsernameProfileData.stars,
+  });
+
+  setLoading(false)
+};
