fbe961a9a1a5ce8fb8239abc08032e469dcfdac2
drakeira <thiago.p.dasilva2005@gmail.com>
2022-12-08 02:53:27 -0300

    feat and refactor: add received github username data test and refactor function

---
+++ src/pages/Main/functions/receiveGithubUserProfileData.spec.ts
@@ -0,0 +1,42 @@
+import { receiveGithubUserProfileData } from "./receiveGithubUserProfileData";
+import { githubApi } from "../../../services/githubApi";
+
+jest.mock('../../../services/githubApi')
+
+const mockedGithubApi = githubApi as jest.Mocked<typeof githubApi>
+
+describe("Received Github User Profile", () => {
+  it("should return an object with model: {login; name; avatar_url; bio;public_repos;followers; following;}", async () => {
+    mockedGithubApi.get.mockResolvedValueOnce({data: {
+        login: 'any_login',
+        name: 'any_name',
+        avatar_url: 'any_url',
+        bio: 'any_bio',
+        public_repos: 10,
+        followers: 10,
+        following: 10
+    }}).mockResolvedValueOnce({data: [
+        {language: 'Typescript'},
+        {language: 'Typescript'},
+        {language: 'Typescript'}
+    ]}).mockResolvedValueOnce({data: [
+        {any: 'any'},
+        {any: 'any'},
+        {any: 'any'},
+    ]})
+    const sut = await receiveGithubUserProfileData("any_username");
+    const mockReturnGithubUsernameData = {
+      name: "any_name",
+      username: "any_login",
+      avatar_url: "any_url",
+      biography: "any_bio",
+      languages: ["Javascript", "PHP", "Haskell"],
+      followers: 10,
+      following: 10,
+      repositories: 10,
+      stars: 3,
+    };
+
+    expect(sut).toEqual(mockReturnGithubUsernameData);
+  });
+});
---
+++ src/pages/Main/functions/receiveGithubUserProfileData.ts
@@ -0,0 +1,39 @@
+import { githubApi } from "../../../services/githubApi";
+
+export const receiveGithubUserProfileData = async (
+  username: string
+): Promise<{}> => {
+  const receivedUsernameProfilePromise = githubApi.get<{
+    login: string;
+    name: string;
+    avatar_url: string;
+    bio: string;
+    public_repos: number;
+    followers: number;
+    following: number;
+  }>(`users/${username}`);
+  const receivedUsernameProgrammingLanguagesPromise = githubApi.get<
+    [{ language: string }]
+  >(`users/${username}/repos`);
+  const receivedUsernameStarredPromise = githubApi.get<[]>(
+    `users/${username}/repos`
+  );
+
+  const response = await Promise.all([
+    receivedUsernameProfilePromise,
+    receivedUsernameProgrammingLanguagesPromise,
+    receivedUsernameStarredPromise,
+  ]);
+
+  return {
+    name: response[0].data.name,
+    username: response[0].data.login,
+    avatar_url: response[0].data.avatar_url,
+    biography: response[0].data.bio,
+    languages: ["Javascript", "PHP", "Haskell"],
+    followers: response[0].data.followers,
+    following: response[0].data.following,
+    repositories: response[0].data.public_repos,
+    stars: response[2].data.length,
+  };
+};
