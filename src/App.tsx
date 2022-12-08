75bd9e7941369d8589a27e31bf841094f745c85d
drakeira <thiago.p.dasilva2005@gmail.com>
2022-12-08 04:27:50 -0300

    feat: add style to main page

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,7 @@ import { MainPage } from "./pages/Main/MainPage"
 
 function App() {
   return (
-    <div className="w-screen h-screen flex justify-center items-center bg-gray-800">
+    <div className="w-screen h-screen flex justify-center items-center bg-gray-900">
       <MainPage data-testid='main-page-component'/>
     </div>
   )
--- src/pages/Main/MainPage.tsx
+++ src/pages/Main/MainPage.tsx
@@ -5,32 +5,47 @@ import { SearchInput } from "../../components/SearchInput/SearchInput";
 import { GithubUserData } from "./functions/receiveGithubUserProfileData";
 import { handleSearchUser } from "./handles/handleSearchUser";
 
-export const MainPage = ({...rest}: React.HTMLAttributes<HTMLDivElement>) => {
+export const MainPage = ({ ...rest }: React.HTMLAttributes<HTMLDivElement>) => {
   const [githubUsernameToSearch, setGithubUsernameToSearch] = useState("");
   const [githubProfileData, setGithubProfileData] = useState<
     GithubUserData | undefined
   >(undefined);
 
   return (
-    <div {...rest}>
-      <header>
-        <span>GitSearch</span>
+    <div {...rest} className="w-full h-full flex  items-center flex-col">
+      <header className="w-full h-[8vh] bg-gray-700 justify-self-start p-5 flex items-center">
+        <span className="text-white lg:text-xl font-bold text-lg">
+          git.Search
+        </span>
       </header>
-      <SearchInput
-        data-testid="search-input-component"
-        onChange={(event) => setGithubUsernameToSearch(event.target.value)}
-        value={githubUsernameToSearch}
-      />
-      <SearchButton
-        data-testid="search-button-component"
-        onClick={() =>
-          handleSearchUser(githubUsernameToSearch, setGithubProfileData)
-        }
-      />
-      <GettedGitHubProfile
-        data-testid="getted-github-profile-component"
-        profileData={githubProfileData}
-      />
+
+      <div className="w-full flex-1 flex justify-center items-center">
+        <main className="w-[80%] h-[60%] lg:w-[60%] rounded-md p-5 flex items-center flex-col shadow-sm">
+          <div className="w-full h-8 flex items-center">
+            <SearchInput
+              data-testid="search-input-component"
+              onChange={(event) =>
+                setGithubUsernameToSearch(event.target.value)
+              }
+              value={githubUsernameToSearch}
+            />
+            <div className="flex-1">
+            <SearchButton
+              data-testid="search-button-component"
+              onClick={() =>
+                handleSearchUser(githubUsernameToSearch, setGithubProfileData)
+              }
+              />
+              </div>
+          </div>
+          <div className="w-full h-[350px] bg-gray-800 rounded-md p-5 flex justify-center items-center">
+            <GettedGitHubProfile
+              data-testid="getted-github-profile-component"
+              profileData={githubProfileData}
+            />
+          </div>
+        </main>
+      </div>
     </div>
   );
 };
