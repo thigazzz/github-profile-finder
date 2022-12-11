1e016d4aa3cf0c3a5497343f9a32a2ee89849a38
drakeira <thiago.p.dasilva2005@gmail.com>
2022-12-11 18:19:49 -0300

    feat: add basic authentication

--- src/routes/routes.tsx
+++ src/routes/routes.tsx
@@ -2,13 +2,19 @@ import { createBrowserRouter } from "react-router-dom";
 import { LoginPage } from "../pages/Login/LoginPage";
 import { MainPage } from "../pages/Main/MainPage";
 
+export const RedirectToLoginPageIUserNotAuth = () => {
+  const token = localStorage.getItem("token");
+
+  return <>{token ? <MainPage /> : <LoginPage />}</>;
+};
+
 export const routes = createBrowserRouter([
-    {
-        path: '/auth/login',
-        element: <LoginPage/>
-    },
-    {
-        path: '/',
-        element: <MainPage/>
-    }
-])
+  {
+    path: "/auth/login",
+    element: <LoginPage />,
+  },
+  {
+    path: "/",
+    element: <RedirectToLoginPageIUserNotAuth />,
+  },
+]);
