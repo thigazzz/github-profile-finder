fd543c6e2e449c04540a18608d317b587c02acc9
drakeira <thiago.p.dasilva2005@gmail.com>
2022-12-11 19:04:08 -0300

    feat: wrap app with auth context

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,17 @@
 import { RouterProvider } from "react-router-dom";
+import { AuthProvider } from "./contexts/auth/AuthContext";
 import { LoadingProvider } from "./contexts/LoadingContext";
 import { routes } from "./routes/routes";
 
 function App() {
   return (
-    <LoadingProvider>
-      <div className="w-screen h-screen flex justify-center items-center bg-gray-900">
-        <RouterProvider router={routes} />
-      </div>
-    </LoadingProvider>
+    <AuthProvider>
+      <LoadingProvider>
+        <div className="w-screen h-screen flex justify-center items-center bg-gray-900">
+          <RouterProvider router={routes} />
+        </div>
+      </LoadingProvider>
+    </AuthProvider>
   );
 }
 
