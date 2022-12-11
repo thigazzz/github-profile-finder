78f87f5939427e5e2f989df9a3df996f49e61202
drakeira <thiago.p.dasilva2005@gmail.com>
2022-12-11 18:25:04 -0300

    feat: add firebase configuration + initialize firebase

---
+++ src/config/firebase.ts
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  apiKey: "AIzaSyD8-4izqCHF8LIRTQVdKzqMys-gY4kGf-k",
+  authDomain: "github-search-2d4be.firebaseapp.com",
+  projectId: "github-search-2d4be",
+  storageBucket: "github-search-2d4be.appspot.com",
+  messagingSenderId: "706565975240",
+  appId: "1:706565975240:web:9ff021c0e6c2504ecd89fd",
+};
---
+++ src/services/firebaseApi.ts
@@ -0,0 +1,4 @@
+import { initializeApp } from "firebase/app";
+import { firebaseConfig } from "../config/firebase";
+
+const app = initializeApp(firebaseConfig);
