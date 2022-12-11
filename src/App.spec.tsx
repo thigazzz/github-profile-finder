938ee856e25950082596143373eb54f9715d9845
drakeira <thiago.p.dasilva2005@gmail.com>
2022-12-11 12:38:25 -0300

    build: add react-router-dom lib

--- src/App.spec.tsx
+++
@@ -1,9 +0,0 @@
-import { render } from "@testing-library/react"
-import App from "./App"
-import { LoadingProvider } from "./contexts/LoadingContext"
-
-test('Render App', () => {
-    const {getByTestId} = render(<App/>, {wrapper: LoadingProvider})
-
-    expect(getByTestId('main-page-component')).toBeInTheDocument()
-})
--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,10 @@
 import { LoadingProvider } from "./contexts/LoadingContext";
-import { MainPage } from "./pages/Main/MainPage";
 
 function App() {
   return (
     <LoadingProvider>
       <div className="w-screen h-screen flex justify-center items-center bg-gray-900">
-        <MainPage data-testid="main-page-component" />
+        
       </div>
     </LoadingProvider>
   );
