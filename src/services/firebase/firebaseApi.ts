1ebfb4fbf51030be176a943e6244d64714d9d977
drakeira <thiago.p.dasilva2005@gmail.com>
2022-12-11 18:30:34 -0300

    feat: add github provider

--- src/services/firebase/firebaseApi.ts
+++ src/services/firebase/firebaseApi.ts
@@ -1,4 +1,4 @@
 import { initializeApp } from "firebase/app";
-import { firebaseConfig } from "../config/firebase";
+import { firebaseConfig } from "../../config/firebase";
 
 const app = initializeApp(firebaseConfig);
