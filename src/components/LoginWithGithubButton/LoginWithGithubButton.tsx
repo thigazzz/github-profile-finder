7a4ceee29e0648efd86d09fb09aa992aad7a4a27
drakeira <thiago.p.dasilva2005@gmail.com>
2022-12-11 18:13:32 -0300

    feat: add style to login page

--- src/components/LoginWithGithubButton/LoginWithGithubButton.tsx
+++ src/components/LoginWithGithubButton/LoginWithGithubButton.tsx
@@ -1,4 +1,5 @@
 import { ButtonHTMLAttributes } from "react";
+import { BsGithub } from "react-icons/bs";
 
 interface LoginWithGithubButtonProps
   extends ButtonHTMLAttributes<HTMLButtonElement> {
@@ -10,9 +11,15 @@ export const LoginWithGithubButton = ({
   ...rest
 }: LoginWithGithubButtonProps) => {
   return (
-    <button {...rest} onClick={onLogin}>
-      <span data-testid="github-icon">Icon</span>
-      Login with Github
+    <button
+      {...rest}
+      onClick={onLogin}
+      className="w-full h-full bg-gray-800 rounded-2xl text-gray-200 flex p-6 items-center hover:bg-gray-700 transition-all"
+    >
+      <BsGithub data-testid="github-icon" className="text-2xl">Icon</BsGithub>
+      <span className="ml-4 font-bold">
+      login with Github
+      </span>
     </button>
   );
 };
--- src/pages/Login/LoginPage.tsx
+++ src/pages/Login/LoginPage.tsx
@@ -2,9 +2,28 @@ import { LoginWithGithubButton } from "../../components/LoginWithGithubButton/Lo
 
 export const LoginPage = () => {
   return (
-    <div>
-      <h2>Find your favorite programming developers</h2>
-      <LoginWithGithubButton data-testid="login-with-github-button-component" />
+    <div className="w-full h-full flex items-center justify-between flex-col sm:flex-row">
+      <section className="w-full h-[30%] bg-gradient-to-b from-green-600 to-gray-900 flex items-center justify-center sm:w-[40%] md:w-[60%] sm:h-full sm:bg-gradient-to-r">
+        <h2 className="text-center text-gray-200 text-xl md:text-2xl">
+          Find your favorite <br />{" "}
+          <span className="font-bold text-2xl md:text-3xl"> programming developers</span>
+        </h2>
+      </section>
+      <section className="flex-1 flex  flex-col w-full p-4">
+        {/* <div className="mt-10 w-full">
+          <span className="font-medium text-gray-200 text-start text-lg">
+            Sign In
+          </span>
+        </div> */}
+        <div className="w-full flex-1 flex items-center justify-center ">
+          <div className="h-20 w-full">
+            <LoginWithGithubButton
+              onLogin={() => console.log()}
+              data-testid="login-with-github-button-component"
+            />
+          </div>
+        </div>
+      </section>
     </div>
   );
 };
