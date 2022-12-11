b7c8460770a1869d4b92478fe26952642e9b6591
drakeira <thiago.p.dasilva2005@gmail.com>
2022-12-11 12:51:14 -0300

    feat: add wrap router-provider in app

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,12 @@
+import { RouterProvider } from "react-router-dom";
 import { LoadingProvider } from "./contexts/LoadingContext";
+import { routes } from "./routes/routes";
 
 function App() {
   return (
     <LoadingProvider>
       <div className="w-screen h-screen flex justify-center items-center bg-gray-900">
-        
+        <RouterProvider router={routes} />
       </div>
     </LoadingProvider>
   );
