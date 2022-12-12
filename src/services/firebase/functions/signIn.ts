3fc0ca204a97905d0e6fb88ac1550a1f7850f439
drakeira <thiago.p.dasilva2005@gmail.com>
2022-12-11 21:45:55 -0300

    fix: change sign with redirect to popup

--- src/services/firebase/functions/signIn.ts
+++ src/services/firebase/functions/signIn.ts
@@ -1,5 +1,34 @@
-import { signInWithRedirect } from "firebase/auth";
+import { GithubAuthProvider, signInWithPopup } from "firebase/auth";
+import { SetStateAction } from "react";
 import { auth } from "../firebaseApi";
 import { githubProvider } from "../githubProvider";
 
-export const signIn = () => signInWithRedirect(auth, githubProvider);
+export const signInWithGithubPopUp = (
+  setUser: React.Dispatch<
+    SetStateAction<{
+      name: string;
+      email: string;
+      url_profile: string;
+    } | null>
+  > | null,
+  setToken: React.Dispatch<SetStateAction<string | null>> | null
+) =>
+  signInWithPopup(auth, githubProvider)
+    .then((result) => {
+      const credential = GithubAuthProvider.credentialFromResult(result);
+      const token = credential?.accessToken;
+      const user = result.user;
+
+      setToken?.(token || null);
+      setUser?.({
+        name: user.displayName || "",
+        email: user.email || "",
+        url_profile: user.photoURL || "",
+      });
+    })
+    .catch((error) => {
+      const errorCode = error.code;
+      const errorMessage = error.message;
+      const email = error.customData.email;
+      const credential = GithubAuthProvider.credentialFromError(error);
+    });
