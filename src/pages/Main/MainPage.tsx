dc140f3e0be22dad0b8a222bcd3433ad7ea17163
drakeira <thiago.p.dasilva2005@gmail.com>
2022-12-12 14:57:15 -0300

    feat: add logout to button

--- src/pages/Main/MainPage.tsx
+++ src/pages/Main/MainPage.tsx
@@ -7,25 +7,29 @@ import { AuthContext, IAuthContext } from "../../contexts/auth/AuthContext";
 import { ILoadingContext, LoadingContext } from "../../contexts/LoadingContext";
 import { GithubUserData } from "./functions/receiveGithubUserProfileData";
 import { handleSearchUser } from "./handles/handleSearchUser";
-import {HiOutlineLogout} from 'react-icons/hi'
+import { HiOutlineLogout } from "react-icons/hi";
+import { useNavigate } from "react-router-dom";
 
 export const MainPage = ({ ...rest }: React.HTMLAttributes<HTMLDivElement>) => {
-  const {user} = useContext(AuthContext) as IAuthContext
-  const {setLoading} = useContext(LoadingContext) as ILoadingContext
+  const { user, logoutWithGithub } = useContext(AuthContext) as IAuthContext;
+  const { setLoading } = useContext(LoadingContext) as ILoadingContext;
+  const navigate = useNavigate();
   const [githubUsernameToSearch, setGithubUsernameToSearch] = useState("");
-  const [githubProfileData, setGithubProfileData] = useState<
-    {
-      githubUserData: GithubUserData | null,
-      errorMessage: {
-        message: string
-      } | null
-    }
-  >({
+  const [githubProfileData, setGithubProfileData] = useState<{
+    githubUserData: GithubUserData | null;
+    errorMessage: {
+      message: string;
+    } | null;
+  }>({
     errorMessage: null,
-    githubUserData: null
+    githubUserData: null,
   });
 
-  console.log(user)
+  const handleLgout = async () => {
+    await logoutWithGithub();
+
+    navigate("/auth/login");
+  };
 
   return (
     <div {...rest} className="w-full h-full flex  items-center flex-col">
@@ -33,16 +37,16 @@ export const MainPage = ({ ...rest }: React.HTMLAttributes<HTMLDivElement>) => {
         <span className="text-white lg:text-xl font-bold text-lg">
           git.Search
         </span>
-        <div className="h-full flex items-center justify-center "> 
-        <LoginOrLogoutWithGithubButton
-              data-testid="login-with-github-button-component"
-            >
-              <div className="flex-1 text-xs sm:text-sm xl:text-lg h-full flex justify-center items-center">
-
+        <div className="h-full flex items-center justify-center ">
+          <LoginOrLogoutWithGithubButton
+            data-testid="login-with-github-button-component"
+            onLogout={handleLgout}
+          >
+            <div className="flex-1 text-xs sm:text-sm xl:text-lg h-full flex justify-center items-center">
               {user?.name}
-              <HiOutlineLogout className="ml-2 text-lg"/>
-              </div>
-            </LoginOrLogoutWithGithubButton>
+              <HiOutlineLogout className="ml-2 text-lg" />
+            </div>
+          </LoginOrLogoutWithGithubButton>
         </div>
       </header>
 
@@ -50,23 +54,26 @@ export const MainPage = ({ ...rest }: React.HTMLAttributes<HTMLDivElement>) => {
         <main className="w-full h-[80%] sm:h-[60%] md:w-[90%] lg:w-[80%] xl:w-[70%] rounded-md p-5 flex items-center flex-col shadow-sm ">
           <div className="w-full h-10 md:h-14 flex items-center">
             <div className="w-full h-full flex-1">
-
-            <SearchInput
-              data-testid="search-input-component"
-              onChange={(event) =>
-                setGithubUsernameToSearch(event.target.value)
-              }
-              value={githubUsernameToSearch}
+              <SearchInput
+                data-testid="search-input-component"
+                onChange={(event) =>
+                  setGithubUsernameToSearch(event.target.value)
+                }
+                value={githubUsernameToSearch}
               />
-              </div>
+            </div>
             <div className="h-full w-10 ml-2 md:w-14">
-            <SearchButton
-              data-testid="search-button-component"
-              onClick={() =>
-                handleSearchUser(githubUsernameToSearch, setGithubProfileData, setLoading)
-              }
+              <SearchButton
+                data-testid="search-button-component"
+                onClick={() =>
+                  handleSearchUser(
+                    githubUsernameToSearch,
+                    setGithubProfileData,
+                    setLoading
+                  )
+                }
               />
-              </div>
+            </div>
           </div>
           <div className="w-full sm:h-[350px] bg-gray-800 rounded-md  p-5 flex justify-center items-center flex-1 mt-2 overflow-hidden">
             <GettedGitHubProfile
